743b593a4f7ed44072af21fd5df577dc80534a7f
DDDHuang <904897578@qq.com>
2021-02-20 15:15:37 +0800

    add: tcp login message

--- apps/aep_tcp/include/aep_tcp.hrl
+++ apps/aep_tcp/include/aep_tcp.hrl
@@ -1,3 +1,19 @@
--define(PASS_THROUGH,"1.0").
--define(NO_PASS_THROUGH,"1.1").
--define(STANDARD_MODULE,"2.0").
+-define(PASS_THROUGH    , "1.0").
+-define(NO_PASS_THROUGH , "1.1").
+-define(STANDARD_MODULE , "2.0").
+
+-define(MESSAGE_TYPE_ERROR          , 0).
+-define(MESSAGE_TYPE_LOGIN          , 1).
+-define(MESSAGE_TYPE_UP             , 2).
+-define(MESSAGE_TYPE_DOWN           , 3).
+-define(MESSAGE_TYPE_UP_ACK         , 0x82).
+-define(MESSAGE_TYPE_DOWN_ACK       , 0x83).
+-define(MESSAGE_TYPE_HEART_BEAT     , 4).
+-define(MESSAGE_TYPE_ACK            , 5).
+-define(MESSAGE_TYPE_HEART_BEAT_ACK , 6).
+
+-define( LOGIN_RESULT_SUCCESS           , 0).
+-define( LOGIN_RESULT_FAIL              , 1).
+-define( LOGIN_RESULT_DEVICE_UNREGISTER , 2).
+-define( LOGIN_RESULT_AUTHENTICATE_FAIL , 3).
+-define( LOGIN_RESULT_ALREADY_LOGIN     , 4).
--- apps/aep_tcp/src/aep_tcp_message_util.erl
+++ apps/aep_tcp/src/aep_tcp_message_util.erl
@@ -8,6 +8,43 @@
 %%%-------------------------------------------------------------------
 -module(aep_tcp_message_util).
 -author("DDDHuang").
+-include("aep_tcp.hrl").
 
 %% API
 -export([]).
+
+register_message(DeviceID, Password, Version) ->
+    DeviceIDLen = length(DeviceID),
+    PasswordLen = length(Password),
+    VersionLen  = length(Version),
+    <<?MESSAGE_TYPE_LOGIN:16, DeviceIDLen:16, DeviceID/binary, PasswordLen:16, Password/binary, VersionLen:16, Version/binary>>.
+register_message_standard_module(DeviceID, Password, Version) ->
+    DeviceIDLen = length(DeviceID),
+    PasswordLen = length(Password),
+    VersionLen  = length(Version),
+    %% wtf
+    Module          = "e_module",
+    ModuleLen       = length(Module),
+    ChipType        = "e_chiptype",
+    ChipTypeLen     = length(ChipType),
+    SoftVersion     = "e_sv",
+    SoftVersionLen  = length(SoftVersion),
+    IMEI            = "e_imei",
+    IMEILen         = length(IMEI),
+    ICCID           = "e_iccid",
+    ICCIDLen        = length(ICCID),
+    IMSI            = "e_imsi",
+    IMSILen         = length(IMSI),
+    StandardModuleInfo =
+        <<  ModuleLen:16, Module/binary,
+            ChipTypeLen:16, ChipType/binary,
+            SoftVersionLen:16, SoftVersion/binary,
+            IMEILen:16, IMEI/binary,
+            ICCIDLen:16, ICCID/binary,
+            IMSILen:16, IMSI/binary
+        >>,
+    <<?MESSAGE_TYPE_LOGIN:16, DeviceIDLen:16, DeviceID/binary, PasswordLen:16, Password/binary, VersionLen:16, Version/binary, StandardModuleInfo/binary>>.
+
+
+
+
